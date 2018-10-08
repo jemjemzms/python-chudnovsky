474ea6ba98d2e5532b34f9eb1b8b52ba2935fe3a
DESKTOP-DRQ7MTF\Jed Diaz <jemjem_zms@hotmail.com>
2018-10-08 23:12:52 +1300

    Aliases and Comments added
    Adding aliases and comments

--- CalcPi.py
+++ CalcPi.py
@@ -1,27 +1,33 @@
+
+"""
+A Python script that will calculate the value of Pi to a set number of decimal places. 
+"""
+
 from decimal import Decimal, getcontext
-import math
-import time
+import math as m
+import time as t
 import colorama
-from colorama import Fore
+from colorama import Fore as f
 colorama.init()
 
 numberofdigits = int(input("Please enter the number of decimal places to calculate Pi to:"))
 getcontext().prec = numberofdigits
 
-start_time = time.time()
-def calc(n):
+start_time = t.time()
+def calc(n): 
     t = Decimal(0)
     pi = Decimal(0)
     deno = Decimal(0)
     k = 0
     for k in range(n):
-        t = (Decimal(-1)**k)*(math.factorial(Decimal(6)*k))*(13591409 + 545140134*k)
-        deno = math.factorial(3*k)*(math.factorial(k)**Decimal(3))*(640320**(3*k))
+        t = (Decimal(-1)**k)*(m.factorial(Decimal(6)*k))*(13591409 + 545140134*k)
+        deno = m.factorial(3*k)*(m.factorial(k)**Decimal(3))*(640320**(3*k))
         pi += Decimal(t)/Decimal(deno)
     pi = pi * Decimal(12)/Decimal(640320**Decimal(1.5))
     pi = 1/pi
     return str(pi)
 
+# run the calculation
 print(calc(1))
-print(Fore.RED + "\nTime taken:", time.time() - start_time)
+print(f.RED + "\nTime taken:", t.time() - start_time)
     
